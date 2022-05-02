b88989b4b5229311dd37568dac1d6e4aa017ad8b
uday2925 <udaykiran1139@gmail.com>
2022-05-02 16:53:50 +0530

    mtd

--- sprint4ce/src/components/Login.jsx
+++ sprint4ce/src/components/Login.jsx
@@ -1,5 +1,5 @@
-import {useState } from 'react';
-import { Navigate } from 'react-router-dom';
+import {useEffect, useState } from 'react';
+import { navigate, useNavigate } from 'react-router-dom';
 import axios from "axios";
 import {useDispatch} from "react-redux"
 import { loggedin } from '../Redux/actions';
@@ -9,10 +9,40 @@ import { loggedin } from '../Redux/actions';
 export const Login = () => {
 
   const dispatch=useDispatch();
+  const navigate=useNavigate();
+  const [username,setUsername]=useState("");
+  const [password,setpassword]=useState("1234");
 
-  const handlebutton=()=>{
-    dispatch(loggedin(true))
+  const  handlechange=(e)=>{
+    if(e.target.name="password")  
+    {
+      setpassword(e.target.value)
+    }
+    
+    if(e.target.name="username")
+    {
+      setUsername(e.target.value)
+    }  
+    
   }
+   
+  const handlebutton=()=>{
+
+    dispatch(loggedin(true))
+    console.log("hi");
+    
+      axios.get(`http://localhost:8080/users/?username=${username}&&pass=${password}`)
+      .then(function (data) {
+              console.log("data1",`http://localhost:8080/users/?username=${username}&&pass=${password}`)
+               
+      }).then(function(data){
+        console.log("data2",data);
+      })
+    
+       
+    
+  }
+  navigate("/orders")
   
 
 
@@ -26,18 +56,20 @@ export const Login = () => {
           type="text"
           name="username"
           placeholder="Enter Username"
-          // onChange={(e)=>{
-          //   handlechange(e);
-          // }}
+          
+          onChange={(e)=>{
+            handlechange(e);
+          }}
         />
         <input
           className="password"
           type="password"
           name="password"
           placeholder="Enter password"
-          // onChange={(e)=>{
-          //   handlechange(e);
-          // }}
+          
+          onChange={(e)=>{
+            handlechange(e);
+          }}
         />
         {/* On this button click make network req to find user with same username and password */}
         {/* get his role, if role is `admin` take him to `/orders` page otherwise take him to `/neworder` */}
